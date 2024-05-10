d3c376d745304e57e22d3cb1bb7ed2800564ca8b
Guillermo <guillermo.gil.cor@gmail.com>
2024-05-10 20:53:47 +0100

    funciona

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const createBox = () => {
   const newElement = document.createElement("div");
-  newElement.innerHTML =`<div onclick="moveBox()" class ="box" id ="${randomId()}">${nId}</div>`;
+  newElement.innerHTML = `<div onclick="moveBox(event)" class ="box" id ="${randomId()}">${nId}</div>`;
   document.body.appendChild(newElement);
 };
-let nId=0;
 
-const randomId = () =>{
-    nId++;
-    return nId;
-}
+let nId = 0;
 
-const moveBox = () =>{
-    const box = document.querySelector("#boxTest")
-}
+const randomId = () => {
+  nId++;
+  return nId;
+};
 
-onmousemove = function(e){console.log(e.clientX, e.clientY)}
+let isMoving = false;
 
+const moveBox = (event) => {
+  const box = document.getElementById(event.target.id);
+  isMoving = true;
+  onmousemove = (e) => {
+    if (!isMoving) return;
+    box.style.top = e.clientY + "px";
+    box.style.left = e.clientX + "px";
+  };
+};
+
+const stopMovingBox = () => {
+  isMoving = false;
+};
 //html colection pasar a array for each selecionar elemento,
